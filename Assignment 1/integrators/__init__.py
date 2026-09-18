2d8caf2f0aa697c91aed0605ccabac1c487e94cd
Abigail Miller <aem337@cornell.edu>
2026-09-17 20:24:23 -0400

    Assignment 2 final

--- Assignment 1/integrators/__init__.py
+++
@@ -1,7 +0,0 @@
-# For more about init files, see https://realpython.com/python-init-py/
-# and https://medium.com/data-science/whats-init-for-me-d70a312da583
-
-from .explicit_euler import explicit_euler
-from .rk4 import rk4
-
-__all__ = ["explicit_euler", "rk4"]
